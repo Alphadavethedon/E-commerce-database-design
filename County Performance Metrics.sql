f0e4be21b0524be45696de7cc056af7928688134
Alphadavethedon <your@email.com>
2025-04-20 21:49:55 +0300

    Update

--- County Performance Metrics.sql
+++
@@ -1,9 +0,0 @@
--- Revenue by county
-SELECT 
-    c.name,
-    COUNT(p.payment_id) AS transactions,
-    SUM(p.amount) AS revenue
-FROM payments p
-JOIN counties c ON p.county_code = c.code
-GROUP BY c.code
-ORDER BY revenue DESC;
